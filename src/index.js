ce27ea7178f8c53a3e14d7cbbde22de5bfe60a18
regular-alves <rodrigo.gois@live.com>
2021-01-23 16:07:28 -0300

    add block twice playing on the same square and pos finishing game plays

--- src/index.js
+++ src/index.js
@@ -48,6 +48,11 @@ class Board extends React.Component {
 
   handleClick(i) {
     const squares = this.state.squares.slice();
+
+    if(calculateWinner(squares) || squares[i]) {
+      return;
+    }
+
     squares[i] = this.state.xIsNext ? 'X' : 'O';
 
     this.setState({
